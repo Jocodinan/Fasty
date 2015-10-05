423e1dc528f1dbf1fc884f285c4ea492be31c72b
Jorge Codina <jorge.codina@gmail.com>
2015-10-05 15:43:07 -0300

    Versión 2.0

---
+++ README.md
@@ -0,0 +1,24 @@
+# Fasty
+Framwork basado en Node.js y Gulp para desarrollar front-end de forma rápida
+
+Para comenzar a trabajar:
+
+1.- Instalar Node.js
+2.- Instalar Ruby
+3.- Instalar Sass
+4.- Instalar Gulp globalmente, en caso de Mac
+5.- Abrir la consola cmd, terminal o powerShell
+6.- Entrar a la carpeta del proyecto ejemplo: $ cd fasty
+7.- Instalar los paquetes de Node: $ npm install
+8.- Iniciar gulp: $ gulp
+
+
+Optimizadore de imágenes:
+
+1.- Para optimizar las imagenes que se encuentran dentro de la carpeta /images, desde el directorio del proyecto: $ gulp images
+
+
+TODOS:
+1.- Limpiador de CSS
+2.- minificador de JS
+3.- minificador de HTML
---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var autoprefixer = require('gulp-autoprefixer');
+var $ = require('gulp-load-plugins')();
+
+var basePath = 'www/';
+var AUTOPREFIXER_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
+
+//----------------------------------------------------------------------PRODUCCION
+
+//Procesa el SASS en el CSS
+gulp.task('styles', function() {
+  return gulp.src([
+    basePath +'sass/main.scss',
+  ])
+  .pipe($.changed('sass', {extension: '.scss'}))
+  .pipe($.rubySass({
+      style: 'expanded',
+      precision: 10
+    })
+    .on('error', console.error.bind(console))
+  )
+  .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+  .pipe($.if('*.css', $.csso())) //optimiza el css
+  .pipe(gulp.dest(basePath +'css'))
+  .pipe($.size({title: 'css'}));
+});
+
+// Lint JavaScript
+gulp.task('jshint', function () {
+  return gulp.src(basePath +'js/main.js')
+    .pipe(reload({stream: true, once: true}))
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
+});
+
+//Tarea que escucha los SCSS y HTML, Tarea para trabajar producción
+gulp.task('default', ['styles'], function () {
+  browserSync({
+    notify: false,
+    server: ['.tmp', basePath]
+  });
+
+  gulp.watch([basePath +'**/*.html'], reload);
+
+  gulp.watch([basePath +'sass/**/*.scss'], ['styles', reload]);
+
+  gulp.watch([basePath +'js/main.js'], ['jshint', reload]);
+  gulp.watch([basePath +'images/**/*'], reload);
+});
+
+
+//Optimiza imagenes en formato PNG y JPG
+gulp.task('images', function () {
+    return gulp.src([basePath +'images/**/*.png', basePath +'images/**/*.jpg'])
+        .pipe($.imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest(basePath +'images/'));
+});
+
